be2629f276a8a8dce20bac5229898591f537ce89
Daniel Varga <daniel.varga@prezi.com>
2015-07-09 11:48:16 +0200

    de-jose

--- setup.sh
+++ setup.sh
@@ -10,9 +10,7 @@ ln -s ~/caffe .
 wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
 mv bvlc_googlenet.caffemodel caffe/models/bvlc_googlenet/
 
-# Get some input images:
-wget --output-document=joseroca.png http://0103.static.prezi.com.s3.amazonaws.com/media/3/4/6/50b6e9d49686c9ba30dfbfe7ce434c4652fd4.png
-convert joseroca.png joseroca.jpg
+# Get some input image:
 wget --output-document=daniel.png https://www.gravatar.com/avatar/17d651b2d4092b7810ae23d8d94b8cbd?s=640
 convert daniel.png daniel.jpg
 
